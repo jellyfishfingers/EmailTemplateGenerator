fed0d46ae1a10d58f6998b69f9c9de513a61855e
Jiayuan <lian.jiayuan@gmail.com>
2022-01-04 01:06:21 +0800

    Added Template File.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace EmailTemplateGenerator
 {
     class Program
     {
+        static void Menu()
+        {
+            Console.WriteLine("Welcome to the application.");
+            Console.WriteLine("Please enter the following:");
+            Console.WriteLine("1 - To create a new template.");
+            Console.WriteLine("E - To exit the program.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string templatefile = System.IO.File.ReadAllText(@"templatefile.txt");
+
+            Menu();
+            string input = Console.ReadLine();
+            if (input=="1") 
+            {
+                Console.WriteLine(templatefile);   
+            }
+        
         }
     }
 }

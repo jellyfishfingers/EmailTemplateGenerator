6e5008ded1e9991b377ae4ede80a0b82bebf10e9
Jiayuan <lian.jiayuan@gmail.com>
2022-01-04 00:10:39 +0800

    Added console project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmailTemplateGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
